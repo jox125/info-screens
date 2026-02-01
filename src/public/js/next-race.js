05af44ad0b866449f7847d1b2e1db34fe40098d2
Joel Lepp <joel.lepp@mail.ee>
2026-02-01 10:25:16 +0200

    added connection warnings

--- src/public/js/next-race.js
+++ src/public/js/next-race.js
@@ -1,19 +1,46 @@
 const socket = io({
   autoConnect: false,
 });
-const raceState = {};
 
-// --- Full screen ---
+const raceState = {};
+const role = "public";
+socket.auth = { role };
+socket.connect();
+
+socket.on("connect_error", (err) => {
+  console.log(err);
+  const warning = document.getElementById("warnings");
+  warning.innerHTML = "Connection error, no realtime data."
+  warning.classList.remove("hidden");
+
+});// --- FULL SCREEN ---
 const screen = document.getElementById("panel");
 const fullScreenButton = document.getElementById("full-screen-button");
 fullScreenButton.onclick = () => {
-  screen.style="border-radius:unset";
+  screen.style = "border-radius:unset";
   screen.requestFullscreen();
-  fullScreenButton.classList.add("hidden");  
+  fullScreenButton.classList.add("hidden");
 };
-document.addEventListener("fullscreenchange", () => {    
-    if (document.fullscreenElement !== screen){        
-        screen.style = "";
-        fullScreenButton.classList.remove("hidden");
-    }
+document.addEventListener("fullscreenchange", () => {
+  if (document.fullscreenElement !== screen) {
+    screen.style = "";
+    fullScreenButton.classList.remove("hidden");
+  }
+});
+
+// --- Connection established ---
+socket.on("auth:ok", (role) => {
+  if (role === "public") {
+
+    // Request initial data
+    socket.emit("state:request");
+    
+    // Hide warnings if exists
+    try {
+      const warning = document.getElementById("warnings");
+      warning.innerHTML = "";
+      warning.classList.add("hidden");
+    } catch (err){}
+
+  }
 });
